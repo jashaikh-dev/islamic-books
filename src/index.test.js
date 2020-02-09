f4ac0d0f2ce178e41a5b2a788763ebbd87e60c1f
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-02-08 23:57:53 -0500

    chore: add travis config and semantic-release

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var islamicBooks = require('./index');
+
+describe('islamic-books', function() {
+    describe('allBooks', function() {
+        it('should be an array of stings', function() {
+            expect(islamicBooks.allBooks).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array){
+                return array.every( function(book) {
+                    return typeof book === 'string'
+                })
+            }
+        })
+
+        it('should contain `Quran`', function() {
+            expect(islamicBooks.allBooks).to.include('Quran');
+        })
+    })
+
+    describe('random book', function() {
+        it('should return a random book from the list', function() {
+            var randomBook = islamicBooks.randomBook();
+            expect(islamicBooks.allBooks).to.include(randomBook);
+        })
+    })
+    
+})
